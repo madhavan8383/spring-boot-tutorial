1481e6798457643de1dae6b939603bb7acedd45b
madhavan8383 <cool24bar7@gmail.com>
2021-05-16 23:21:19 +0530

    data source updated

--- springboot2-jdbc-crud-mysql-example/src/main/resources/data.sql
+++ springboot2-jdbc-crud-mysql-example/src/main/resources/data.sql
@@ -1,3 +1,2 @@
-insert into employees values(10001,'Ramesh', 'Fadatare', 'ramesh@gmail.com');
-
-insert into employees values(10002,'Jit', 'Jadhav', 'jadhav@gmail.com');
+insert into production_dept.employees values(10001,'Ramesh', 'Fadatare', 'ramesh@gmail.com');
+insert into production_dept.employees values(10002,'Jit', 'Jadhav', 'jadhav@gmail.com');
